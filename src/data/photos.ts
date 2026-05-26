dd2cef323e2f6e36dc01fe9bc5ea48d64d975f54
hyunseo-s <son020120@gmail.com>
2026-05-26 22:40:50 +1000

    updated image details

--- src/data/photos.ts
+++ src/data/photos.ts
@@ -25,9 +25,9 @@ export const photos: Photo[] = [
       "/images/photos/IMG_2572.jpg"
     ],
     "thumbnail": "/images/photos/IMG_1622.jpg",
-    "title": "Summer Collection",
-    "category": ["Featured", "Nature", "Summer"],
-    "description": "A collection of shots from the peak of summer, showcasing both nature and featured moments."
+    "title": "Cats and Dogs",
+    "category": ["Seokgu", "Nim", "Milo"],
+    "description": "Furries hehe"
   },
   {
     "id": "featured-carousel-2",
@@ -37,134 +37,92 @@ export const photos: Photo[] = [
       "/images/photos/IMG_2874.jpg"
     ],
     "thumbnail": "/images/photos/IMG_2810.jpg",
-    "title": "Urban Series",
-    "category": ["Featured", "Urban", "Series"],
-    "description": "A multi-part series exploring urban textures and street life."
+    "title": "Cat's being stupid",
+    "category": ["Nim", "Milo"],
+    "description": "Why are they so stupid"
   },
   {
     "id": "1",
     "url": "/images/photos/54A64A44-364A-42BD-999B-AE76A78C6E10.jpg",
     "thumbnail": "/images/photos/54A64A44-364A-42BD-999B-AE76A78C6E10.jpg",
-    "title": "Photo 1",
-    "category": "Misc"
+    "title": "Side eye",
+    "category": "Theo"
   },
   {
     "id": "2",
     "url": "/images/photos/D6D0213F-DA2E-476B-B243-144ADE10EF41.jpg",
     "thumbnail": "/images/photos/D6D0213F-DA2E-476B-B243-144ADE10EF41.jpg",
-    "title": "Photo 2",
-    "category": "Misc"
-  },
-  {
-    "id": "3",
-    "url": "/images/photos/IMG_1622.jpg",
-    "thumbnail": "/images/photos/IMG_1622.jpg",
-    "title": "Photo 3",
-    "category": "Misc"
-  },
-  {
-    "id": "4",
-    "url": "/images/photos/IMG_2449.jpg",
-    "thumbnail": "/images/photos/IMG_2449.jpg",
-    "title": "Photo 4",
-    "category": "Misc"
-  },
-  {
-    "id": "5",
-    "url": "/images/photos/IMG_2572.jpg",
-    "thumbnail": "/images/photos/IMG_2572.jpg",
-    "title": "Photo 5",
-    "category": "Misc"
-  },
-  {
-    "id": "6",
-    "url": "/images/photos/IMG_2810.jpg",
-    "thumbnail": "/images/photos/IMG_2810.jpg",
-    "title": "Photo 6",
-    "category": "Misc"
-  },
-  {
-    "id": "7",
-    "url": "/images/photos/IMG_2859.jpg",
-    "thumbnail": "/images/photos/IMG_2859.jpg",
-    "title": "Photo 7",
-    "category": "Misc"
-  },
-  {
-    "id": "8",
-    "url": "/images/photos/IMG_2874.jpg",
-    "thumbnail": "/images/photos/IMG_2874.jpg",
-    "title": "Photo 8",
-    "category": "Misc"
+    "title": "Hello there!",
+    "category": "Theo"
   },
   {
     "id": "9",
     "url": "/images/photos/IMG_2937.jpg",
     "thumbnail": "/images/photos/IMG_2937.jpg",
-    "title": "Photo 9",
-    "category": "Misc"
+    "title": "Quokka",
+    "category": "Nature"
   },
   {
     "id": "10",
     "url": "/images/photos/IMG_2959.jpg",
     "thumbnail": "/images/photos/IMG_2959.jpg",
-    "title": "Photo 10",
-    "category": "Misc"
+    "title": "Long neck",
+    "category": "Nature"
   },
   {
     "id": "11",
     "url": "/images/photos/IMG_2968.jpg",
     "thumbnail": "/images/photos/IMG_2968.jpg",
-    "title": "Photo 11",
-    "category": "Misc"
+    "title": "Ooga Booga",
+    "category": "Nature"
   },
   {
     "id": "12",
     "url": "/images/photos/IMG_2993.jpg",
     "thumbnail": "/images/photos/IMG_2993.jpg",
-    "title": "Photo 12",
-    "category": "Misc"
+    "title": "Prety boy",
+    "category": "Theo"
   },
   {
     "id": "13",
     "url": "/images/photos/IMG_3056.jpg",
     "thumbnail": "/images/photos/IMG_3056.jpg",
-    "title": "Photo 13",
-    "category": "Misc"
+    "title": "Warm cat",
+    "category": "Nim"
   },
   {
     "id": "14",
     "url": "/images/photos/IMG_3060.jpg",
     "thumbnail": "/images/photos/IMG_3060.jpg",
-    "title": "Photo 14",
-    "category": "Misc"
+    "title": "Moments before disaster",
+    "category": ["Nim", "Milo"],
   },
   {
     "id": "15",
     "url": "/images/photos/IMG_3093.jpg",
     "thumbnail": "/images/photos/IMG_3093.jpg",
-    "title": "Photo 15",
-    "category": "Misc"
+    "title": "Travels",
+    "category": "Nim"
   },
   {
     "id": "16",
     "url": "/images/photos/att.EPXVq3UjKiL-8G2ZH1oUWsV4vKwdJpgNanpsU79qPDU.jpg",
     "thumbnail": "/images/photos/att.EPXVq3UjKiL-8G2ZH1oUWsV4vKwdJpgNanpsU79qPDU.jpg",
-    "title": "Photo 16",
-    "category": "Misc"
+    "title": "Smiley boy",
+    "category": "Theo"
   },
   {
     "id": "17",
     "url": "/images/photos/att.dfhucOSSarpfxZn1XeEW20kyupXd2-P5dlASpiZOv-Q.jpg",
     "thumbnail": "/images/photos/att.dfhucOSSarpfxZn1XeEW20kyupXd2-P5dlASpiZOv-Q.jpg",
-    "title": "Photo 17",
-    "category": "Misc"
+    "title": "Happy birthday",
+    "category": "Theo"
   },
   {
     "id": "18",
     "url": "/images/photos/att.fDmz4rVrK6rJHbhuzJGnX4q611WK3T80GOspRuvCEuQ.jpg",
     "thumbnail": "/images/photos/att.fDmz4rVrK6rJHbhuzJGnX4q611WK3T80GOspRuvCEuQ.jpg",
-    "title": "Photo 18",
-    "category": "Misc"
+    "title": "Birthday gnome",
+    "category": "Theo"
   }
 ];
